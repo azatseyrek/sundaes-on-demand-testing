43ffaa9bf462b4a64caee4dfaeb737b9d597d011
azatseyrek <azatseyrek@hotmail.com>
2022-09-30 16:25:28 +0300

    mock service worker added

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,40 +1,57 @@
-const {render, screen, fireEvent} = require('@testing-library/react');
-const {default: SummaryForm} = require('../SummaryForm');
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
-describe('SummaryForm', () => {
-  test('Inital conditions', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: / Terms and Conditions/i,
-    });
-    expect(checkbox).not.toBeChecked();
-
-    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
-    expect(confirmButton).toBeDisabled();
+test('Initial conditions', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
   });
+  expect(checkbox).not.toBeChecked();
 
-  test('Checkbox disables button on first click and enables on second click', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: /terms and conditions/i,
-    });
-    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
+  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
+  expect(confirmButton).toBeDisabled();
+});
 
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
   });
+  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-  test('Checkbox enables button on first click and disables on second click', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: /terms and conditions/i,
-    });
-    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
 
-    fireEvent.click(checkbox); //2nd click
-    expect(confirmButton).toBeDisabled();
-  });
+test('popover responds to hover', async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    //expect yerine bunu kullandik cunku react render icin bekliyor ve expect ile bekleyemiyoruz. bu yuzden expect yerine waitForElementToBeRemoved kullandik.
+
+    screen.queryByText(/no ice cream will actually be delivered/i),
+  );
 });
