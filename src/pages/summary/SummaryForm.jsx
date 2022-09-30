538d64ac471d75b719e419f122ef1295bbab6a6d
azatseyrek <azatseyrek@hotmail.com>
2022-09-30 09:57:25 +0300

    summary form added

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,23 +1,52 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
-const SummaryForm = () => {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <label htmlFor="checkbox" name="checkbox">
-        terms and conditions
-      </label>
-      <input
-        id="checkbox"
-        type="checkbox"
-        name="checkbox"
-        aria-checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
-      />
+export default function SummaryForm({setOrderPhase}) {
+  const [tcChecked, setTcChecked] = useState(false);
 
-      <button disabled={!checked}>confirm order</button>
-    </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase('completed');
+  }
+
+  const popover = (
+    <Popover id="termsandconditions-popover">
+      <Popover.Content>No ice cream will actually be delivered</Popover.Content>
+      {/* note: for React Bootstrap 2.x, the previous line needs to be:
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+      (replace Popover.Content with Popover.Body). For more details, see 
+      https://www.udemy.com/course/react-testing-library/learn/lecture/30126784*/}
+    </Popover>
   );
-};
 
-export default SummaryForm;
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{color: 'blue'}}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+}
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,36 +1,40 @@
 const {render, screen, fireEvent} = require('@testing-library/react');
 const {default: SummaryForm} = require('../SummaryForm');
 
-test('Inital conditions', () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', {name: 'terms and conditions'});
-  expect(checkbox).not.toBeChecked();
+describe('SummaryForm', () => {
+  test('Inital conditions', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', {
+      name: / Terms and Conditions/i,
+    });
+    expect(checkbox).not.toBeChecked();
 
-  const confirmButton = screen.getByRole('button', {name: 'confirm order'});
-  expect(confirmButton).toBeDisabled();
-});
-
-test('Checkbox disables button on first click and enables on second click', () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', {
-    name: /terms and conditions/i,
+    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
+    expect(confirmButton).toBeDisabled();
   });
-  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-  fireEvent.click(checkbox);
-  expect(confirmButton).toBeEnabled();
-});
+  test('Checkbox disables button on first click and enables on second click', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-test('Checkbox enables button on first click and disables on second click', () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', {
-    name: /terms and conditions/i,
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
   });
-  const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-  fireEvent.click(checkbox);
-  expect(confirmButton).toBeEnabled();
+  test('Checkbox enables button on first click and disables on second click', () => {
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole('checkbox', {
+      name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole('button', {name: /confirm order/i});
 
-  fireEvent.click(checkbox); //2nd click
-  expect(confirmButton).toBeDisabled();
+    fireEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+
+    fireEvent.click(checkbox); //2nd click
+    expect(confirmButton).toBeDisabled();
+  });
 });
